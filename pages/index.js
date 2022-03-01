c2e66f0e8f1885ee621b14cfa3f42565010710f5
fazil <fnahnah@gmail.com>
2022-03-01 23:09:11 +0100

    faz-boutique_v1

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import {
+  Grid,
+  Typography,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+} from '@material-ui/core';
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home() {
   return (
     <Layout>
       <div>
-        <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-          <li>Product 4</li>
-        </ul>
+        <Typography variant="h2">List Products</Typography>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>$ {product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
